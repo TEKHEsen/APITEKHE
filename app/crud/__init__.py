3061b1c8213bfaa45724e681c2ae32f0dc998bfb
Papa Seydou WANE <seydouwane2011@gmail.com>
2026-01-10 00:20:16 +0000

    crud_patient import patient

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -3,3 +3,4 @@ from .crud_visite import crud_consultation as consultation
 from .crud_patient import crud_patient
 from .crud_visite import crud_consultation
 from .crud_user import crud_user as user 
+from .crud_patient import patient
